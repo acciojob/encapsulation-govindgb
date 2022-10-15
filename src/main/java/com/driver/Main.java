1307f8b0bce34bc573b7c0a54da8b135002b1adf
govindgb <govindbhattpnd@gmail.com>
2022-10-15 10:45:56 +0530

    encapsulaton

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+      RWOnly obj = new RWOnly();
+
+      //obj.name = "govind";
+      //java: name has private access in com.driver.RWOnly
+      obj.setName("govind");
+      System.out.println(obj.getName());
+
+
+    }
 }
